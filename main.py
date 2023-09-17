bfe8d1b97c927dec7d367df1595c565a893a0cc3
Kilsup-Lee (이길섭) <gislee@dgu.ac.kr>
2023-09-17 22:58:12 +0900

    s5

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 print("Hello World!")
-name = 'Kilsup'
+name = 'Kilsup Lee'
 print('I am {0}'.format(name))
 programming_language = 'Python'
 print('Most used language is {0}'.format(programming_language))
