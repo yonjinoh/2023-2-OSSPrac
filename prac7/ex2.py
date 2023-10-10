b1452349da88c9ab71a35a04a2a8ad655828e516
way4data <gislee@dgu.ac.kr>
2023-10-10 11:44:57 +0900

    add prac7

---
+++ prac7/ex2.py
@@ -0,0 +1,23 @@
+# 파이썬 파일이 직접 스크립트로 실행될 때만 flask 애플리케이션을 실행 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return ('Hello World')
+
+@app.route('/page')  # page URL
+def print_page():
+    return ('This is page')
+
+if __name__ == '__main__':
+    app.run()
+
+# 만약?
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
+# 만약?
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
